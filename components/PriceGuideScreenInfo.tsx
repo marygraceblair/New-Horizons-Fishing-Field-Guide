0f21f9fefc64f3f7ed9b2739b7bfe5edf3998903
MaryGrace Blair <mblair021@gmail.com>
2020-07-29 14:18:34 -0500

    added basic UI outline'

--- components/PriceGuideScreenInfo.tsx
+++ components/PriceGuideScreenInfo.tsx
@@ -7,83 +7,97 @@ import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
 import { TextInput } from 'react-native';
-
-
+import { Input, CheckBox, Button } from 'react-native-elements';
 export default function PriceGuideScreenInfo({ path }: { path: string }) {
-  const [value, onChangeText] = React.useState('Critter Name:');
-  return (
-    <View>
-      <View style={styles.getStartedContainer}>
-      </View>
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
-    </View>
-  );
+    const [value, onChangeText] = React.useState('Critter Name:');
+    return (
+        <View>
+            <View style={styles.getStartedContainer}>
+            </View>
+            <Input
+                label='Name'
+                placeholder='Critter Name'
+                errorStyle={{ color: 'red' }}
+                errorMessage='ENTER A VALID NAME'
+            />
+            <CheckBox
+                title='Bug'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+            />
+            <CheckBox
+                title='Fish'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+            />
+            <Button
+                title='Find Price'
+                type='outline'
+            />
+        </View>
+    );
 }
 
 function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet'
-  );
-}
+    WebBrowser.openBrowserAsync(
+        'https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet'
+      );
+    }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  helpContainer: {
-    marginTop: 15,
-    marginHorizontal: 20,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    textAlign: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+      },
+    developmentModeText: {
+        marginBottom: 20,
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center',
+    },
+    contentContainer: {
+        paddingTop: 30,
+    },
+    welcomeContainer: {
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    welcomeImage: {
+        width: 100,
+        height: 80,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginLeft: -10,
+    },
+    getStartedContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50,
+    },
+    homeScreenFilename: {
+        marginVertical: 7,
+    },
+    codeHighlightText: {
+        color: 'rgba(96,100,109, 0.8)',
+    },
+    codeHighlightContainer: {
+        borderRadius: 3,
+        paddingHorizontal: 4,
+    },
+    getStartedText: {
+        fontSize: 17,
+        lineHeight: 24,
+        textAlign: 'center',
+    },
+    helpContainer: {
+        marginTop: 15,
+        marginHorizontal: 20,
+        alignItems: 'center',
+    },
+    helpLink: {
+        paddingVertical: 15,
+    },
+    helpLinkText: {
+        textAlign: 'center',
+    },
 });
